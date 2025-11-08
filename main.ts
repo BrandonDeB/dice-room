fae088188d2371deeed4d0d8899f8f239d5aa841
BrandonDeB <brandonrdebaroncelli@gmail.com>
2025-11-07 23:26:22 -0600

    Dice rolling plugin  - Join rooms  - Change personal preferences  - Roll dice and show to the room  - Display the results

--- main.ts
+++ main.ts
@@ -1,34 +1,39 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, WorkspaceLeaf, Modal, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { DiceView, VIEW_TYPE_DICE } from 'view';
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface DiceRoomPluginSettings {
+	serverAddress: string;
+	displayName: string;
+	frontColor: string;
+	backColor: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: DiceRoomPluginSettings = {
+	serverAddress: 'ws://localhost:8765',
+	displayName: 'Anonymous',
+	frontColor: '#ffffff',
+	backColor: '#000000'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class DiceRoomPlugin extends Plugin {
+	settings: DiceRoomPluginSettings;
+	ws: WebSocket | null = null;
+	reconnectTimeout: number | null = null;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		this.registerView(
+			VIEW_TYPE_DICE,
+			(leaf) => new DiceView(leaf, this)
+		);
+
+		this.addRibbonIcon('dice', 'Dice Roller', () => {
+			this.activateView();
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
 
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'open-sample-modal-simple',
 			name: 'Open sample modal (simple)',
@@ -36,51 +41,36 @@ export default class MyPlugin extends Plugin {
 				new SampleModal(this.app).open();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, _view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		this.addSettingTab(new DiceRollSettingTab(this.app, this));
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
-
 	}
+
+	async activateView() {
+		const { workspace } = this.app;
+
+		let leaf: WorkspaceLeaf | null = null;
+		const leaves = workspace.getLeavesOfType(VIEW_TYPE_DICE);
+
+
+		if (leaves.length > 0) {
+
+			leaf = leaves[0];
+			leaf.detach();
+		} else {
+
+			leaf = workspace.getRightLeaf(false);
+			if (leaf != null) {
+				await leaf.setViewState({ type: VIEW_TYPE_DICE, active: true });
+				workspace.revealLeaf(leaf);
+			}
+		}
+	}
+
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -107,10 +97,10 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class DiceRollSettingTab extends PluginSettingTab {
+	plugin: DiceRoomPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: DiceRoomPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,14 +111,44 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Backend Server Address')
+			.setDesc('Put address to host')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter the address')
+				.setValue(this.plugin.settings.serverAddress)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.serverAddress = value;
 					await this.plugin.saveSettings();
 				}));
+
+		new Setting(containerEl)
+			.setName('Display Name')
+			.setDesc('The name that will show to other room members')
+			.addText(text => text
+				.setPlaceholder('Enter your name')
+				.setValue(this.plugin.settings.displayName)
+				.onChange(async (value) => {
+					this.plugin.settings.displayName = value;
+				}));
+
+		new Setting(containerEl)
+			.setName('Dice Background Color')
+			.setDesc('Main color of dice')
+			.addColorPicker(value =>  { value
+				.setValue(this.plugin.settings.frontColor)
+				.onChange(async (value) => {
+					this.plugin.settings.frontColor = value;
+				})
+			})
+
+		new Setting(containerEl)
+			.setName('Dice Foreground Color')
+			.setDesc('Text Color on Dice')
+			.addColorPicker(value =>  { value
+				.setValue(this.plugin.settings.backColor)
+				.onChange(async (value) => {
+					this.plugin.settings.backColor = value;
+				})
+			})
 	}
 }
---
+++ view.ts
@@ -0,0 +1,211 @@
+import { ItemView, WorkspaceLeaf } from 'obsidian';
+import DiceBox from '@3d-dice/dice-box-threejs';
+import { DiceNotation } from '@3d-dice/dice-box-threejs/src/DiceNotation.js';
+import DiceRoomPlugin from "./main";
+
+export const VIEW_TYPE_DICE = 'dice-view';
+let currentUser = 0;
+
+export class DiceView extends ItemView {
+	plugin: DiceRoomPlugin;
+	rollers: Map<number, DiceBox>;
+	container: HTMLElement;
+	ws: WebSocket;
+	room: string;
+
+	constructor(leaf: WorkspaceLeaf, plugin: DiceRoomPlugin) {
+		super(leaf);
+		this.plugin = plugin;
+		this.container = this.contentEl.createDiv({cls: 'dice-parent'})
+		this.rollers = new Map();
+
+		this.ws = new WebSocket(`${this.plugin.settings.serverAddress}`);
+
+		this.ws.onmessage = (e) => {
+			const data = JSON.parse(e.data);
+
+			if (data.type == "roll") {
+				const notation = data.results;
+				this.rollers.get(data.user).roll(notation);
+			} else if (data.type == "join") {
+				const rollView = document.getElementById('scene-container');
+				if (rollView != null && rollView.instanceOf(HTMLDivElement)) {
+					this.createDiceObject(data.user, rollView);
+				}
+			} else if (data.type == "leave") {
+				this.removeUser(data.user);
+			} else if (data.type == "join_ack") {
+				this.container.empty();
+				this.container.createEl('h2', { text: this.room });
+				const textInp = this.container.createEl('input', { text: 'Dice View', type: 'text' });
+				const btn = this.container.createEl('button', { text: 'Roll', type: 'button' });
+				const leave = this.container.createEl('button', { text: 'Leave Room', type: 'button' });
+				currentUser = data.user_id;
+
+				btn.addEventListener('click', () => {
+					const results = new DiceNotation(textInp.value, true);
+
+					this.ws.send(JSON.stringify({
+						"type": "roll",
+						"user": currentUser,
+						"results": results.stringify(true),
+						"room":  this.room,
+					}));
+
+				});
+
+				leave.addEventListener('click', () => {
+					this.ws.send(JSON.stringify({
+						"type": "leave",
+						"id": currentUser,
+						"room":  this.room,
+					}));
+
+					currentUser = 0;
+					this.room = "";
+					this.onOpen();
+
+				});
+
+				textInp.addEventListener("keypress", function(event) {
+					if (event.key === "Enter") {
+						event.preventDefault();
+						btn.click();
+					}
+				});
+
+				requestAnimationFrame(() => {
+					this.createDiceObjects(data.users);
+				})
+			}
+		}
+	}
+
+	getViewType() {
+		return VIEW_TYPE_DICE;
+	}
+
+
+	getDisplayText() {
+		return 'Dice View';
+	}
+
+	getRollAreaId(user:number) {
+		return `roll-area-${user}`;
+	}
+
+	getIcon() {
+		return "dice"
+	}
+
+	createPopup(parent: HTMLDivElement, user: number) {
+		const popup = parent.createDiv({cls: 'popup', attr: {id: `popup-${user}`}});
+		const closeButton = popup.createEl("span", {text: "×", cls: 'close-btn'});
+		const popupText = popup.createEl("p");
+		const popupHeader = popup.createEl("h4", {text: "Roll Results"});
+		popup.style.display = "none";
+		popupText.id = `popup-text-${user}`;
+		closeButton.onclick = () => {
+			popup.style.display = "none";
+		}
+
+		popup.appendChild(closeButton);
+		popup.appendChild(popupHeader);
+		popup.appendChild(popupText);
+
+		return popup;
+	}
+
+	private createDiceBox(user: number, frontColor: string, backColor: string) {
+		return new DiceBox(`#${this.getRollAreaId(user)}`, {
+			theme_customColorset: {
+				background: backColor,
+				foreground: frontColor,
+				material: "metal" // metal | glass | plastic | wood
+			},
+			light_intensity: 1,
+			gravity_multiplier: 400,
+			baseScale: 100,
+			strength: 8,
+			onRollComplete: (results) => {
+
+				const popupText = document.getElementById(`popup-text-${user}`);
+				if (popupText != null) {
+					popupText.textContent = `${results.notation}=${results.total}`;
+				}
+				const userPopup = document.getElementById(`popup-${user}`);
+				if (userPopup != null) {
+					userPopup.style.display = "block";
+
+					// setTimeout(() => {
+					// 	userPopup.style.display = "none";
+					// }, 5000);
+				}
+			}
+		});
+
+	}
+
+	private removeUser(user: number) {
+		let roller = this.rollers.get(user);
+		if (roller) {
+			roller = null;
+			this.rollers.delete(user);
+		}
+		const elem = document.getElementById(this.getRollAreaId(user));
+		if (elem && elem.parentElement) {
+			elem.parentElement.removeChild(elem);
+		}
+	}
+
+	private createDiceObject(user: {id: number, name: string, frontColor: string, backColor: string}, rollView: HTMLDivElement) {
+		const ua = rollView.createDiv({cls: "single-roller", attr: {id: this.getRollAreaId(user.id)}});
+		ua.createEl("h3", {cls: "player-label", text: user.name});
+		this.createPopup(ua, user.id);
+
+		const Box = this.createDiceBox(user.id, user.frontColor, user.backColor);
+		Box.initialize();
+		this.rollers.set(user.id, Box);
+	}
+
+	private createDiceObjects(users: {id: number, name: string, frontColor: string, backColor: string}[]) {
+		const rollView = this.container.createDiv({ cls: 'rollView', attr: {id: "scene-container"} });
+		users.forEach(user => {
+			this.createDiceObject(user, rollView);
+		});
+	}
+
+	joinRoom(room: string) {
+		this.ws.send(JSON.stringify({
+			"type": "join",
+			"name": this.plugin.settings.displayName,
+			"room": room,
+			"frontColor": this.plugin.settings.frontColor,
+			"backColor": this.plugin.settings.backColor,
+		}))
+		this.room = room;
+	}
+
+	async onOpen() {
+
+		this.container.empty();
+		this.container.createEl('h2', { text: 'Join a Room' });
+		const textInp = this.container.createEl('input', { text: 'Room Name', type: 'text' });
+		const btn = this.container.createEl('button', { text: 'Join', type: 'button' });
+
+		btn.addEventListener('click', () => {
+			this.joinRoom(textInp.value);
+		});
+
+		textInp.addEventListener("keypress", function(event) {
+			if (event.key === "Enter") {
+				event.preventDefault();
+				btn.click();
+			}
+		});
+	}
+
+	async onClose() {
+		this.ws?.close();
+	}
+}
